cb9c968d2b94d3f77dbc4eb6c0a4bebf12d5d50c
harriskrem <harriskremm@outlook.com>
2024-04-09 19:17:46 +0300

    small func added

--- app.ts
+++ app.ts
@@ -1,15 +1,15 @@
 import express from 'express';
-import { createServer } from 'node:http';
+import { createServer } from 'node:https';
 import { Server } from 'socket.io';
 import fs from 'fs';
 
-// const options = {
-//   key: fs.readFileSync('/home/harriskr/certs/practice/server.key'), // replace it with your key path
-//   cert: fs.readFileSync('/home/harriskr/certs/practice/server.crt'), // replace it with your certificate path
-// };
+const options = {
+  key: fs.readFileSync('C:\\Users\\harri\\certs\\vite_localhost\\server.key'), // replace it with your key path
+  cert: fs.readFileSync('C:\\Users\\harri\\certs\\vite_localhost\\server.crt'), // replace it with your certificate path
+};
 
 const app = express();
-const server = createServer(app);
+const server = createServer(options, app);
 const io = new Server(server);
 
 
@@ -17,31 +17,38 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
+let connections: string[] = [];
+
 io.on('connection', (socket) => {
   console.log('a user connected: ', socket.id);
 
-  socket.on('signal', (data) => {
-    const { id, message } = data;
+  for (const [key] of io.sockets.sockets) {
+    connections.push(key);
+  }
 
-    const connections: string[] = [];
-    for (let [key] of io.sockets.sockets) {
-      connections.push(key);
-    }
+  socket.on('share_id', ({ peerId }: { peerId: string }) => {
+    socket.to(peerId).emit('get_id', { peerId: socket.id });
+  });
 
-    if (id && connections.includes(id)) {
-      console.log("connections: ", connections);
-      socket.broadcast.to(id).emit("data", message);
-    } else {
-      console.log("Wrong ID. Here are the connections: ", connections);
-    }
+  socket.on('send_candidate', ({ candidate, peerId }: { candidate: unknown, peerId: string }) => {
+    socket.to(peerId).emit("get_candidate", { candidate, peerId: socket.id });
+  });
+
+  socket.on('send_connection_offer', ({ offer, peerId }: { offer: RTCSessionDescriptionInit, peerId: string }) => {
+    socket.to(peerId).emit('get_connection_offer', { offer, peerId: socket.id });
+  });
+
+  socket.on('answer', ({ answer, peerId }: { answer: RTCSessionDescriptionInit, peerId: string }) => {
+    socket.to(peerId).emit('get_answer', { answer, peerId: socket.id });
   });
 
   // Handle disconnection
   socket.on('disconnect', () => {
+    connections = connections.filter(connection => connection !== socket.id);
     console.log('A user disconnected');
   });
 
-  // for debugging purposes
+  // for debugging purposes 
   socket.onAny((eventName, ...args) => {
     console.log(eventName); // 'hello'
     console.log(args); // [ 1, '2', { 3: '4', 5: ArrayBuffer (1) [ 6 ] } ]
@@ -56,5 +63,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log('server running at http://localhost:3000');
+  console.log('server running at https://localhost:3000');
 });
